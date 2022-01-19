0eaadf5cf196a545550163c0893b90e7b08481f7
ChandanH123 <chandanhayaran@gmail.com>
2022-01-19 16:05:47 +0530

    yoyoyo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,14 @@ import { Error404Component } from './error404/error404.component';
 import { StatesTableComponent } from './states-table/states-table.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard/states_table', pathMatch: 'full' },
+  { path: '404', redirectTo: 'dashboard/states_table', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent,
     children: [
       {path: 'states_table',component:StatesTableComponent},
       {path: 'districts_table',component:DistrictsTableComponent},
   ]},
-  {path: '404', component: Error404Component},
-  {path: '**', redirectTo: '/404'}
+  // {path: '404', component: Error404Component},
+  // {path: '**', redirectTo: '/404'}
 ];
 
 
