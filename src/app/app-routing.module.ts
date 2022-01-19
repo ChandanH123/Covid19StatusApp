4198a960538f5ccdcf84d5324c60d6b7f35ddfd1
ChandanH123 <chandanhayaran@gmail.com>
2022-01-19 16:15:48 +0530

    dsgjdgjs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DistrictsTableComponent } from './districts-table/districts-table.component';
-import { Error404Component } from './error404/error404.component';
 import { StatesTableComponent } from './states-table/states-table.component';
 
 const routes: Routes = [
